0884cb3d77e2c94d036a46ee44453aee7d65e2e4
Vaxo Aroshidze <aroshidze.vakho@gmail.com>
2023-12-06 15:16:51 +0400

    create module

---
+++ om_hospital/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Hospital Management',
+    'version': '1.0.0',
+    'category': 'Hospital',
+    'sequence': -100,
+    'summary': 'Hospital management system',
+    'description': """Hospital management system""",
+    'depends': [],
+    'data': [],
+    'demo': [],
+    'application': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}        
