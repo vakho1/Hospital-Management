2ecf0152bd60360d03a298b66c6257e09655ffcd
Vaxo Aroshidze <aroshidze.vakho@gmail.com>
2023-12-25 20:57:49 +0400

    [MV2-220] Create Computed Field

--- om_hospital/models/patient.py
+++ om_hospital/models/patient.py
@@ -1,5 +1,5 @@
+from datetime import date
 from odoo import api, fields, models
-
 
 class HospitalPatient(models.Model):
     _name = "hospital.patient"
@@ -7,7 +7,17 @@ class HospitalPatient(models.Model):
     _description = "Hospital Patient"
 
     name = fields.Char(string='Name', tracking=True)
+    date_of_birth = fields.Date(string='Date of Birth')
     ref = fields.Char(string='Reference', default='Odoo')
-    age = fields.Integer(string="Age", tracking=True)
+    age = fields.Integer(string="Age", compute='_compute_age', tracking=True)
     gender = fields.Selection([('male', 'Male'), ('female', 'Female')], string="Gender", tracking=True, default='female')
     active = fields.Boolean(string="Active", default=True)
+
+    @api.depends('date_of_birth')
+    def _compute_age(self):
+        for rec in self:   
+            today = date.today()
+            if rec.date_of_birth:    
+                rec.age = today.year - rec.date_of_birth.year 
+            else:
+                rec.age = 0    
