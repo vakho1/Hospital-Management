23dae2d398921fc9e65c75bba1b6e9c1d45b99d3
Vaxo Aroshidze <aroshidze.vakho@gmail.com>
2024-01-03 18:49:57 +0400

    41 and 42 tasks done

--- om_hospital/models/appointment.py
+++ om_hospital/models/appointment.py
@@ -24,6 +24,8 @@ class HospitalAppointment(models.Model):
         ('done', 'Done'), 
         ('cancel', 'Cancelled')], default='draft', string="Status", required=True)
     
+    doctor_id = fields.Many2one("res.users", string="Doctor")
+
     @api.onchange('patient_id')
     def onchange_patient_id(self):
         self.ref = self.patient_id.ref
